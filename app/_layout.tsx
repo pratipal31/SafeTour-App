21e9cc84a6c5a02315f1c5fd2fc0f266cab78723
pratipal31 <pratipal.dhaulakhandi.31@gmail.com>
2025-09-30 13:28:56 +0530

    Reapply "Reapply "first commit""
    This reverts commit 7ea68d68c4c8ded380aeb73731b0e4498a98d5e2.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,17 @@ import '../global.css';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return <Stack >
+    <Stack.Screen 
+    name="(tabs)"
+    options={{headerShown: false,
+    }}
+    />
+
+    <Stack.Screen
+    name="movie/[id]"
+    options={{headerShown: false,}} />
+
+    </Stack>
+
 }
