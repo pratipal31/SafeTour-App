108d76d515d83be60c38556f1937d549336c301b
pratipal31 <pratipal.dhaulakhandi.31@gmail.com>
2025-09-30 13:26:00 +0530

    Revert "first commit"
    This reverts commit 7a88cbf66b1f2338027ca15f75d46eb855b76101.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,7 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {
-      colors: {
-        primary :"#030014",
-        secondary:"#151312",
-        light:{
-          100:"D6C6FF",
-          200:"A8B5DB",
-          300:"9CA4AB"
-        },
-        dark:{
-          100:"#221f3d",
-          200:"#0f0d23",
-        },
-        accent:"#AB8BFF"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
