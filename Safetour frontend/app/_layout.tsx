e7a26c30282f550c9059e3e26504714df5c541ba
pratipal31 <pratipal.dhaulakhandi.31@gmail.com>
2025-10-03 23:53:59 +0530

    made a lil change on tabs

--- Safetour frontend/app/_layout.tsx
+++ Safetour frontend/app/_layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{headerShown: false}}/>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(home)" options={{ headerShown: false }} />
       </Stack>
     </ClerkProvider>
   );
